93c0e28b3bd4b269674955db12c4a2fb07f050de
ussamaawan <91910265+ussamaawan@users.noreply.github.com>
2022-09-18 22:02:33 +0500

    Update solution.py

--- Q3/solution.py
+++ Q3/solution.py
@@ -1,3 +1,18 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
+# first we need to sort two arrays using time complexity
+a=[] 
+b=[]
+# using for loop and time
+for each x in b
+      insert x into a
+   or q from 1 to m
+      if q == 1 then insert b[q] into a
+      else 
+         insert b[q] into a starting from the position of b[q-1]
+            return length
+        #Now we have merged array length we can easily find median for odd and even case
+        length =len (merged_array)
+        if length % 2 == 0 :
+            med = (merged_array[(length/2)-1] + merged_array [length/2]/2
+                   else :
+                   med =merged_array [int(length/2)]
+                   
