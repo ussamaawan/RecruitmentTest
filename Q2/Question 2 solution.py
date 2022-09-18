b16b7c09d3218fd67f5570add7a880c29421305b
ussamaawan <91910265+ussamaawan@users.noreply.github.com>
2022-09-18 21:28:58 +0500

    Update and rename solution.py to Question 2 solution.py

---
+++ Q2/Question 2 solution.py
@@ -0,0 +1,17 @@
+#first we need to find the size  for that we are introducing a function
+def len_link(list):
+    temp=list.head
+    count=0
+    while(temp):
+        count+=1
+        temp=temp.next
+    return count
+ # then from that size value we will use for calculating head
+    size = len .link(optional)
+    temp= size-n
+    # using for loop
+    for i in range (0,size):
+        if i == temp:
+            optional .remove(i)
+            return optional.head
+        
